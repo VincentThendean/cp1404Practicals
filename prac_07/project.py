7ef7ece8dc1b41ff0f220063ea4012d402f327fd
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2023-12-11 15:39:18 +0800

    Add initial project.py, project_management.py and project.txt

---
+++ prac_07/project.py
@@ -0,0 +1,19 @@
+"""
+Project
+Estimate: 2 hours
+1)  Start: 15:30
+    End:
+Actual
+"""
+
+
+class Project:
+
+    def __init__(self, name, start_date, priority, cost):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion = 0
+
+
