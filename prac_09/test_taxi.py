ec48a8805a70342215b74c1cc16110cfac18d38e
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2024-01-06 13:58:10 +0800

    Finished test_taxi.py

---
+++ prac_09/test_taxi.py
@@ -0,0 +1,15 @@
+from car import Car
+from taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
