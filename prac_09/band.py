811777b58cf433e408c932f6043dc6012dbf31c2
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2024-01-06 17:31:31 +0800

    Finish band.py

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,24 +7,28 @@ class Band:
 
     def __str__(self):
         """Return a string representation of a Band."""
-        return f"{self.name} ({self.players})"
+        return f"{self.name} ({str(self.players).strip('[').strip(']')})"
 
     def __repr__(self):
         """Return a string representation of a Band, showing the variables."""
-        return str(vars(self))
+        return str(self)
 
     def add(self, player):
         """Add a player to band's ensemble."""
         self.players_instruments[player.name] = player.instruments
-        self.players.append(player)
+        self.players.append(f"{player.name}, {player.instruments}")
 
     def play(self):
-        for i in range(3):
-            return self.players[i].play()
+        for player in self.players_instruments:
+            # print(player)
+            if not self.players_instruments[player]:
+                print(f"{player} needs an instrument!")
+            else:
+                print(f"{player} is playing: {str(self.players_instruments[player]).strip('[').strip(']')}")
+        return ""
 
-
-if __name__ == '__main__':
-    from musician import Musician
-
-    band = Band()
-
+#
+# if __name__ == '__main__':
+#     from musician import Musician
+#
+#     band = Band()
