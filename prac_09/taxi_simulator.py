6daa321e17d20e8caf6a11d3808db2970a228810
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2024-01-11 17:08:35 +0800

    Add drive taxi function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -16,8 +16,15 @@ def main():
     while menu_choice != "q":
         if menu_choice == "c":
             get_taxi_list(taxis)
-            set_current_taxi(taxis)
+            current_taxi = set_current_taxi(taxis)
             get_current_bill(current_bill)
+            print(current_taxi)
+        elif menu_choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                drive_taxi(current_taxi, taxis)
+                current_bill = add_current_bill(current_bill, current_taxi, taxis)
 
         else:
             print("Invalid option")
@@ -27,14 +34,26 @@ def main():
         menu_choice = menu_choice.lower()
 
 
+def add_current_bill(current_bill, current_taxi, taxis):
+    current_bill += taxis[current_taxi].get_fare()
+    get_current_bill(current_bill)
+    return current_bill
+
+
+def drive_taxi(current_taxi, taxis):
+    drive_distance = int(input("Drive how far? "))
+    taxis[current_taxi].drive(drive_distance)
+    print(f"Your trip cost you {taxis[current_taxi].get_fare():.2f}")
+
+
 def get_current_bill(current_bill):
     print(f"Bill to date: ${current_bill:.2f}")
 
 
 def set_current_taxi(taxis):
     try:
-        taxi_choice = int(input("Choose taxi: "))
-        current_taxi = taxis[taxi_choice]
+        current_taxi = int(input("Choose taxi: "))
+        return current_taxi
     except ValueError:
         print("Not a number")
     except IndexError:
