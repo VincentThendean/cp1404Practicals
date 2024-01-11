d8b3187445da43553c07ac601538fb7b82e68e6c
Vincent Thendean <vincent.thendean@my.jcu.edu.au>
2024-01-11 16:36:00 +0800

    Add main menu

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,15 @@
+def main():
+
+    print("Let's drive!")
+    print("q)uit, c)hoose taxi, d)rive")
+    menu_choice = input(">>> ")
+    menu_choice = menu_choice.lower()
+
+    while menu_choice != "q":
+
+        print("Let's drive!")
+        print("q)uit, c)hoose taxi, d)rive")
+        menu_choice = input(">>> ")
+        menu_choice = menu_choice.lower()
+
+main()
